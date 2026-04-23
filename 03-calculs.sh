4710de0afd22505a30b84ec60c37fa2cbc68f415
Emmanuel <e.eblouinaguila2@etu.cvm.qc.ca>
2026-04-23 09:11:03 -0400

    Notes calculs

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+echo 10+10          #Affiche en string "10+10"
+echo $((10+10))     #sans espace! (affiche résultat de 10+10)
+
+#Affiche également le résultat en storant dans une variable
+read -p "Entrez un nombre : " nb1
+read -p "Un autre nombre : " nb2
+let resultat=$nb1*$nb2
+echo $resultat
