5529368dc09ef5dbe7e836032e4b6f865879e4f2
Emmanuel <e.eblouinaguila2@etu.cvm.qc.ca>
2026-04-23 09:53:47 -0400

    Notes regex

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+#Façon A
+echo -n "Texte : "
+read choix
+#Façon B
+#read -p "Texte : " choix
+
+if [[ $choix =~ ^[0-9]+$ ]]     #~ signifie une comparaison par regex
+then
+    echo "nombre"
+fi
