d8e2169c876004bd9932ae416717736b86617228
Emmanuel <e.eblouinaguila2@etu.cvm.qc.ca>
2026-04-23 09:48:14 -0400

    Notes fonction

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 
+fonction1(){
+    echo "Info kernel " `uname -rs`
+    echo "param = $1"
+}
+
+fonction1 Testing       #Affichera "param = Testing"
+
+fonction1 parm1 parm2   #$1 $2 pour afficher "parm1 parm2"
