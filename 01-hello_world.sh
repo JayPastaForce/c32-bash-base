c3577d2ceb250a6316014ca3d5e67d1bb916960b
Emmanuel <e.eblouinaguila2@etu.cvm.qc.ca>
2026-04-23 08:58:36 -0400

    Notes hello world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+# exit
+echo "Hello world"
+
+echo -n Vive
+echo "Linux"
+
+#Écalue les méta-caractères
+echo -e "123\n456"
+
+#pas de $ quand on affecte
+#en lecture de la variable , mettre $ avant
+read -p "Quel âge avez-vous?" age
+echo $age " ans!"
