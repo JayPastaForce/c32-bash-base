e7fee1ba1c087a21528506fc4103352eb14099c6
Emmanuel <e.eblouinaguila2@etu.cvm.qc.ca>
2026-04-23 10:21:40 -0400

    exercice2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+choix=
+check=
+
+while [[ $choix != q ]]
+do
+    clear
+    echo "==========="
+    echo "- Menu 1 -"
+    echo "-----------"
+    read -p "Entez une option " choix
+    if [[ $choix = m ]]
+    then
+        read -p "Quel mot voulez-vous vérifier? " check
+        ./exercice1.sh $check
+        check=
+        sleep 3
+    fi
+    #Si le choix est m, alors demmander un mot au clavier puis appeler exercice1.sh
+done
