975732be6f4e201d772f620ab266e82bc2b17e4d
Emmanuel <e.eblouinaguila2@etu.cvm.qc.ca>
2026-04-23 09:40:22 -0400

    Notes boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))     #Écrit comme en C ou Java... c'est une exception
+do
+    echo $i
+done
+
+listFichier=`ls`        #` ` Évalue une commande
+
+for fichier in $listFichier
+do
+    echo $fichier
+done
+
+choix=
+
+while [[ $choix != q ]]
+do
+    clear
+    echo "==========="
+    echo "- Menu 1 -"
+    echo "-----------"
+    read -p "Entez un choix (q)" choix
+done
